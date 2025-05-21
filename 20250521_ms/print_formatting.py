bba037246eb996d035ee45fb947ea369557b4353
AbrarTunio <abrar.tunio@yahoo.com>
2025-05-21 21:14:48 +0500

    formatting

---
+++ 20250521_ms/print_formatting.py
@@ -0,0 +1,10 @@
+name   = "Masharib"
+gender = "Male"
+age    = 21
+
+
+print("My name is",name,"and I am", gender, "and I am" , age, "years old" )
+print("My name is {} and i am {} and I am {} years old".format(name , gender, age)  )
+
+
+print(f"My name is {name} and I am {gender} and I am {age} years old")
