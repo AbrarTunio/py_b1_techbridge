38a09729b34dfca09ee2dedfcfa806549e0025f8
AbrarTunio <abrar.tunio@yahoo.com>
2025-05-21 21:31:48 +0500

    arithmetic operations

---
+++ 20250521_ms/arithmetic.py
@@ -0,0 +1,27 @@
+# Arithmetic Operators
+
+# 1- Addition                  +
+# 2- Subtraction               -
+# 3- Multiplication            *
+# 4- Division                  /
+# 5- Power / Exponent          **
+# 6- Remainder                 %
+# 7- Quotient                  //
+
+ratePerDay = 800
+days = 30
+
+monthlySalary = ratePerDay * days
+
+print( f"The salary of labor is {monthlySalary}" )
+
+
+num1 = 5
+num2 = 7
+
+# answer = num1 * num2
+answer = num1 / num2
+# answer = num1 - num2
+# answer = num1 + num2
+print( answer )
+
