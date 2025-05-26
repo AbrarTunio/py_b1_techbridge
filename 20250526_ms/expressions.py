ee90135ac0ee6ac685e9aad8761cec2d5b19c57d
AbrarTunio <abrar.tunio@yahoo.com>
2025-05-26 14:47:57 +0500

    Input and Type conversion

---
+++ 20250526_ms/expressions.py
@@ -0,0 +1,37 @@
+# name = input("Enter your name: ")
+
+# print(name)
+# print( type( name )   )
+
+# age = input( "Enter your age: " )
+
+# print(age )
+# print( type( age ) )
+
+# PROBLEM IN CALCULATION FOR STRING NUMBERS
+# Contacatenation 
+
+# num1 = "5"
+# num2 = "10"
+
+# print( int( num1 ) +  int( num2 ) )  #type conversion str into int
+
+
+# a = input("Enter any number1")
+# b = input("Enter any number2")
+
+# print( int( a ) + int( b ) )
+
+
+# x =  int( input("how much amount do you have in pocket? ") )
+# y = int(  input("How much amount is in Sir Abrar's pocket")  )
+
+# total_amount = x + y
+
+# print( total_amount )
+
+
+name = "Masharib "
+copies = 5
+
+print( name * copies  )
