0144cfd29ea24d1302ed3a351da80c8f331f7f6a
AbrarTunio <abrar.tunio@yahoo.com>
2025-05-29 14:55:54 +0500

    You must work hard

---
+++ 20250529_ms/condition.py
@@ -0,0 +1,40 @@
+print("Conditional Operations")
+##########################3
+
+# Types of Condtional Operations
+
+# 5  >  6  #False
+# 5  <  6  #True
+
+# 6 > 6  #False
+# 9 < 9  #False
+
+# 6 >= 6 # True
+# 9 >= 6 # True
+# 9 <= 6 # False
+
+# 6 <= 9   # True
+# 21 < 20  # True
+
+# 6 == 6    # True
+# print( 6 == "6" ) # False
+
+# print( 5 != 6 )   #True
+
+# print( 15 != "15" )
+
+# print( "a" > "M" )
+# print( ord("A") )
+
+
+a = int ( input("Enter your first number: "))
+b = int (input("Enter your second number: "))
+
+if ( a > b ):
+	print( "First number",a,"is greater than",b  )
+
+print("Wah Bhai Chal Gaya tu")
+
+
+
+
