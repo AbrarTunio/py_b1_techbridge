e5fa83d6aecc924d8e22c0ff9c5d9fa8028180d7
AbrarTunio <abrar.tunio@yahoo.com>
2025-06-12 21:23:55 +0500

    Loops and if else

---
+++ 20250612_ms/loop.py
@@ -0,0 +1,17 @@
+# for idx in range(5):
+# 	print("Now counting:", idx)
+
+# for name in [ "Masharib" , "Awais" , "Sana" , "Abrar" ]:
+# 	print("Salam to:", name)
+
+# names = [ "Masharib" , "Awais" , "Sana" , "Abrar" ]
+# for name in names:
+# 	print("Salam to:", name)
+
+
+names = [ "Masharib" , "Awais" , "Sana" , "Abrar" ]
+for name in names:
+	if name == "Sana":
+		print( name , "is a girl")
+	else:
+		print( name , "is a boy")
