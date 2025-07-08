5eeba1dd73b93d4906dd53628be27ed650fbb8e7
AbrarTunio <abrar.tunio@yahoo.com>
2025-07-08 20:15:37 +0500

    turtle

---
+++ 20250701/function.py
@@ -0,0 +1,7 @@
+def greet(name):
+    return f"Welcome Big Boss {name}" 
+
+uname = input("Who is big boss? ")
+result = greet(uname)
+
+print( result )
---
+++ 20250701/re.py
@@ -0,0 +1,21 @@
+# def square(n):
+#     return int(n)**2
+# # ******************************
+
+# num = input( "Provide a number to make square" )
+# print( square(num) )
+
+def sumTwo( a , b=0 ):
+    return f"The sum of two {int(a) + int(b)}"
+
+# num1 = input("Enter 1st number: ")
+# num2 = input("Enter 2nd number: ")
+
+# print( sumTwo( num1 , num2 ) )
+
+
+message = "good morning"
+print( message.upper() )
+
+newMsg = "Good Night"
+print( newMsg.lower() )
