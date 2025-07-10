702d00d8f6dafd311377e9688ea1b2a07e2f7b09
AbrarTunio <abrar.tunio@yahoo.com>
2025-07-10 19:59:30 +0500

    list and geting data

---
+++ 20250710/list.py
@@ -0,0 +1,20 @@
+fruits = [ 'Apple' , 'Banana' , 'Ornage' , 'Kiwi' ]
+numbers = [ 15 , 12 , 22 , 19 , 99 ]
+
+general = [ 'students' , 15 , 18.8 , [] , True ]
+
+university = [  
+          ['telecom' , 55 ] , 
+          ['CSE' ,      95 ] ,
+          ['Software', 120 ]  
+    ]
+
+print( fruits[0] )
+print( numbers[1] )
+print( general[-1] )
+
+print( fruits[1:4] )
+print( fruits[1:] )
+print( fruits[:2] )
+
+print( university[2][0] )
