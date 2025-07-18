e95db6ffc50c5e81a5d3b90fb0c3a2f9dcb45543
AbrarTunio <abrar.tunio@yahoo.com>
2025-07-18 12:53:53 +0500

    list methods

---
+++ 20250718_ms/listm.py
@@ -0,0 +1,20 @@
+vege = [ 'tomato' , 'potato' , 'onion' , 'potato' , 'ladyfinger' , 'chilli' ]
+
+# vege.append('cucumber')
+# vege.extend( ['cucumber' , 'shimla' , 'ginger'] ) 
+# print( len( vege )  ) 
+# vege.insert( 2 , 'cucumber' )
+# vege.remove('potato')
+# vege.pop(1)
+# print( vege )
+
+# pCount = vege.count('potato')
+# print( pCount )
+
+# vege.sort()
+newVege = vege.copy()
+
+print(vege)
+print('----------')
+newVege.clear()
+print(newVege)
