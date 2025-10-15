e8008aa4b35929e83dc3c9c714facd419117f18d
AbrarTunio <abrar.tunio@yahoo.com>
2025-10-15 19:24:18 +0500

    new batch

---
+++ 20251015/abrar_l2.py
@@ -0,0 +1,15 @@
+# message = "TechBridge Institute and CodePros"  #sting data
+# print( message )
+
+num1 = 15
+num2 = 19
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2 
+
+print("Sum of two", add )
+print("Diff of two", sub )
+print("Prod of two", mul )
+print("Division of two", div )
