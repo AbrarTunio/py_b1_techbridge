76d01bf175adf3719b60982072ec2823c9902d5e
AbrarTunio <abrar.tunio@yahoo.com>
2025-11-11 14:48:23 +0500

    sets

---
+++ 20251110/listvsset.py
@@ -0,0 +1,12 @@
+emptyList = []  #list()
+emptyDict = {}  #dict()
+emptyTuple = () #tuple()
+emptySet = set() 
+
+# emptyList.append(1)
+# emptyDict['count'] = 1
+# emptyTuple = (9,) #This is insertion not append or adding
+emptySet.add(9)
+
+emptySet.update([20, 23])
+print(emptySet)
